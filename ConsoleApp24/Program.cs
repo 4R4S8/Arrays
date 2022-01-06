f47b6cadf6d6bba7191aeace3a7337b260216165
4R4S8 <arashzomorrodi24@gmail.com>
2022-01-06 23:10:31 +0330

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+Console.Write("Enter The Length of the Array: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+int[] number=new int[x];
+Console.WriteLine();
+for (int j = 0; j < number.Length; j++)
+{
+    Console.Write("Enter The Number: ");
+    y = int.Parse(Console.ReadLine());
+    number[j] = y;
+}
+//Console.WriteLine();
+//foreach (int  g in number)
+//{
+//    Console.WriteLine(" {0} ",g);
+
+//}
+
+//Console.WriteLine();
+
+//Console.WriteLine(number.Max());
+//Console.WriteLine(number.Min());
+//Console.WriteLine(number.Sum());
+//Console.WriteLine(number.Count());
+//Array.Sort(number);
